4bc9935bb5c20cbe03a20f283e892584f9d82ab1
bousoutennshi <bousou.tenshi6017@gmail.com>
2024-05-29 03:20:32 +0900

    init

---
+++ src/components/Popover.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { PopoverContent, PopoverPortal, PopoverRoot, PopoverTrigger } from 'radix-vue'
+</script>
+
+<template>
+  <PopoverRoot>
+    <PopoverTrigger>ここを押すと</PopoverTrigger>
+    <PopoverPortal>
+      <PopoverContent>
+        さらなるコンテンツが表示されます
+      </PopoverContent>
+    </PopoverPortal>
+  </PopoverRoot>
+</template>
